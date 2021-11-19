13cc120b8edf5d45a5da26fa5580c69b99c0d843
Joe Flateau <joe@joeflateau.net>
2021-11-19 12:47:47 -0500

    labeled tuples rather than objects

--- src/index.ts
+++ src/index.ts
@@ -6,26 +6,26 @@ export function fitRect(
   target: Rect,
   mode: FitMode = "contain"
 ): Rect {
-  const sw = target.w / rect.w;
-  const sh = target.h / rect.h;
-  let scale = 1;
+  const sw = target[2] / rect[0];
+  const sh = target[3] / rect[1];
 
+  let scale = 1;
   if (mode == "contain") {
     scale = Math.min(sw, sh);
   } else if (mode == "cover") {
     scale = Math.max(sw, sh);
   }
 
-  return {
-    x: target.x + (target.w - rect.w * scale) / 2,
-    y: target.y + (target.h - rect.h * scale) / 2,
-    w: rect.w * scale,
-    h: rect.h * scale,
-  };
+  return [
+    target[0] + (target[2] - rect[0] * scale) / 2,
+    target[1] + (target[3] - rect[1] * scale) / 2,
+    rect[0] * scale,
+    rect[1] * scale,
+  ];
 }
 
-export type Size = { w: number; h: number };
+export type Size = [w: number, h: number];
 
-export type Rect = { x: number; y: number; w: number; h: number };
+export type Rect = [x: number, y: number, w: number, h: number];
 
 export type FitMode = "contain" | "cover";
